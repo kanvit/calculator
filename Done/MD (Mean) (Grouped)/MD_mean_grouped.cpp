9f13eae0ae2268d9c46ef04b2c7b5233e86b8c6f
Kanvit <demonsevillieutenant13@gmail.com>
2019-07-02 19:38:34 +0530

    function of mean deviation about mean for grouped data

--- Done/MD (Mean) (Grouped)/MD_mean_grouped.cpp
+++ Done/MD (Mean) (Grouped)/MD_mean_grouped.cpp
@@ -6,7 +6,8 @@
 using namespace std;
 
 double absolute(double x);
-double MD_mean(int classes);
+double MD_mean_grouped(int classes);
+
 main(void) {
 	int classes;
 	double meanDeviationAboutMean;
@@ -15,7 +16,7 @@ main(void) {
 	cout << "Enter number of classes: ";
 	cin >> classes;
 
-	meanDeviationAboutMean = MD_mean(classes);
+	meanDeviationAboutMean = MD_mean_grouped(classes);
 
 	cout << "Mean Deviation about Mean: " << meanDeviationAboutMean;
 
@@ -23,13 +24,9 @@ main(void) {
 
 }
 
-double MD_mean(int classes) {
-	double sum_mean_deviation = 0;
-
+double MD_mean_grouped(int classes) {
 /* DYNAMIC ALLOCATION OF ARRAYS lower_limit, upper_limit, x */
-	double *lower_limit = new double[classes];
-	double *upper_limit = new double[classes];
-	double *x = new double[classes];
+	double *lower_limit = new double[classes], *upper_limit = new double[classes], *x = new double[classes];
 
 /* PROMPT FOR LOWER LIMIT AND UPPER LIMIT OF THE FIRST CLASS */
 	cout << "Enter:\n";
@@ -52,9 +49,7 @@ double MD_mean(int classes) {
 
 /* DYNAMIC ALLOCATION OF ARRAYS frequency, cumulative_frequency, sumfx AND fx BASED ON THE VALUE OF classes */
 	int *frequency = new int[classes];
-	double *fx = new double[classes];
-	double *cumulative_frequency = new double[classes];
-	double *sumfx = new double[classes];
+	double *fx = new double[classes], *cumulative_frequency = new double[classes], *sumfx = new double[classes];
 
 	cumulative_frequency[0] = 0;
 	sumfx[0] = 0;
@@ -71,10 +66,7 @@ double MD_mean(int classes) {
 
 
 /* EVALUATION OF mean */
-	double mean = sumfx[classes] / cumulative_frequency[classes];
-
-
-	double *mean_deviation = new double[classes];
+	double mean = sumfx[classes] / cumulative_frequency[classes], *mean_deviation = new double[classes], sum_mean_deviation = 0;
 	for(int i = 1; i <= classes; i++) {
 		mean_deviation[i] = x[i] - mean;
 
@@ -83,10 +75,6 @@ double MD_mean(int classes) {
 
 	double mean_deviation_mean = sum_mean_deviation / cumulative_frequency[classes];
 	return mean_deviation_mean;	
-/* DEALLOCATION OF THE ARRAYS */
-	delete lower_limit, upper_limit, x, fx, sumfx, cumulative_frequency, frequency;
-
-	getch();
 }
 
 double absolute(double x) {
