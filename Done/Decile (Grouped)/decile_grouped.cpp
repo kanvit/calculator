44f6f2bd052297a759c2e580a58033e7e1230a04
Kanvit <demonsevillieutenant13@gmail.com>
2019-07-02 12:52:28 +0530

    function of decile of grouped data

--- Done/Decile (Grouped)/decile_grouped.cpp
+++ Done/Decile (Grouped)/decile_grouped.cpp
@@ -6,19 +6,26 @@
 
 using namespace std;
 
+void decile_grouped(int classes);
+
 main(void) {
-/* PROMPT FOR NUMBER OF CLASSES */
 	int classes;
+/* PROMPT FOR NUMBER OF CLASSES */
 	cout << "Enter number of classes: ";
 	cin >> classes;
 
+	decile_grouped(classes);
+
+	getch();
+	return 0;
+}
+
+void decile_grouped(int classes) {
 /* DYNAMIC ALLOCATION OF ARRAYS lower_limit AND upper_limit BASED ON THE VALUE OF classes */
-	double *lower_limit = new double[classes];
-	double *upper_limit = new double[classes];	
+	double *lower_limit = new double[classes], *upper_limit = new double[classes];	
 
 /* PROMPT FOR THE LOWER LIMIT AND UPPER LIMIT OF THE FIRST CLASS */
-	cout << "Enter:\n";
-	cout << "Lower Limit of the first class: ";
+	cout << "Enter:\nLower Limit of the first class: ";
 	cin >> lower_limit[1];
 
 	cout << "Upper Limit of the second class: ";
@@ -54,14 +61,13 @@ main(void) {
 /* LOOP TO COMPUTE THE VALUE OF decobs */
 	for(int i = 1; i < 10; i++) {
 		decobs = (i * cumulative_frequency[classes]) / 10;
-				
+					
 /* LOOP TO EVALUATE THE DECILE */
 		for(int j = 1; j <= classes; j++) {
 			if(decobs <= cumulative_frequency[j]) {
 				obslower_limit = lower_limit[j];
 
-				decile[i] = obslower_limit + (((decobs - cumulative_frequency[j - 1]) / frequency[j]) * class_size);
-				
+				decile[i] = obslower_limit + (((decobs - cumulative_frequency[j - 1]) / frequency[j]) * class_size);				
 				break;
 			}
 		}
@@ -69,25 +75,8 @@ main(void) {
 
 	system("cls");
 
-/* FORMATTING WHICH DISPLAYS THE DATA IN TABULAR FORM */
-	cout << endl << "|\tclass\t|\tf\t|\tc.f.\t|\n";
-	for(int j = 1; j <= classes; j++)
-	{
-		cout << "|\t" << lower_limit[j] << " - " << upper_limit[j] << "\t|\t";
-		cout << frequency[j] << "\t|\t" << cumulative_frequency[j] << "\t|" << endl;
-
-	}
-
-/* LOOP TO DISPLAY THE DECILES */
+/* LOOP TO DISPLAY THE DECILES*/
 	cout << "\n\n|\tnumber\t|\tdecile\t|\n";
 	for(int j = 1; j < 10; j++)
-	{
 		cout << "|\t" << j << "\t|\t" << decile[j] << "\t|" << endl;
-	}
-
-
-/* DEALLOCATION OF THE VARIABLES */
-	delete lower_limit, upper_limit, decile, frequency, cumulative_frequency;
-
-	getch();
 }
