f113b0da1b45af52474e5f67e8f2a4b62e2a5dd4
Kanvit <demonsevillieutenant13@gmail.com>
2019-07-03 10:36:32 +0530

    function of variance of discrete data

--- Done/Variance (Ungrouped)/variance_discrete.cpp
+++ Done/Variance (Ungrouped)/variance_discrete.cpp
@@ -1,4 +1,4 @@
-/* VARIANCE OF UNGROUPED DATA */
+/* VARIANCE OF discrete DATA */
 
 #include <conio.h>
 #include <iostream>
@@ -6,30 +6,26 @@
 
 using namespace std;
 
-double variance_ungrouped(int classes);
+double variance_discrete(int classes);
 
 main(void) {
-/* DECLARATION OF VARIABLES */
 	int classes;
 
-/* PROMPT FOR THE NUMBER OF CLASS MARKS */
 	cout << "Enter number of class marks: ";
 	cin >> classes;
 
-	double variance = variance_ungrouped(classes);
+	double variance = variance_discrete(classes);
 	cout << "Variance: " << variance << endl << "Standard Deviation: " << sqrt(variance) << endl;
 
 	getch();
 	return 0;
 }
 
-double variance_ungrouped(int classes) {
-/* DYNAMIC ALLOCATION OF ARRAY x BASED ON THE VALUE OF classes */
+double variance_discrete(int classes) {
 	double *x = new double[classes];
 	cout << "Enter the values of x:\n";
 
 	double sum_x = 0;
-/* LOOP TO STORE THE VALUE OF CLASS MARKS */
 	for(int j = 1; j <= classes; j++) {
 		cout << j << ". ";
 		cin >> x[j];
@@ -43,7 +39,6 @@ double variance_ungrouped(int classes) {
 	for(int j = 1; j <= classes; j++) {
 		deviation_x[j] = x[j] - mean_x;
 		sum_square_deviation_x += pow(deviation_x[j], 2);
-
 	}
 
 	double variance = sum_square_deviation_x / classes;
